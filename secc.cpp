980ff229cf46a04a5f77c943bef1acff26d61f66
yONg <ivere27@gmail.com>
2016-10-03 23:32:17 +0900

    prototype of using SimpleHttpRequest and nlohmann/json
    don't use the c++restsdk/boost(heavy) library anymore.
    
    FIXME : need to refactor a lot

--- secc.cpp
+++ secc.cpp
@@ -1,7 +1,7 @@
 /*
   SECC Native(c++) Frontend
 
-  dependencies : c++ rest sdk(aka casablanca), zlib, md5
+  dependencies : SimpleHttpRequest, nlohmann/json, zlib, md5
   ubuntu :
   $ sudo apt-get install libcpprest-dev libssl-dev zlib1g-dev
   $ make clean && make
@@ -11,19 +11,18 @@
 */
 
 #include <iostream>
-#include <cpprest/http_client.h>
-#include <cpprest/producerconsumerstream.h>
+#include "json.hpp"
+#include "SimpleHttpRequest.hpp"
+
 #include <sys/utsname.h>
 
-#include "log.h"
 #include "untar.h"
 #include "utils.h"
 #include "zip.h"
 
 using namespace std;
-using namespace web;                        // Common features like URIs.
-using namespace web::http;                  // Common HTTP functionality
-using namespace concurrency::streams;
+using namespace request;
+using namespace nlohmann;
 
 class _secc_exception : public std::exception
 {
@@ -43,34 +42,35 @@ int main(int argc, char* argv[])
     LOGI(cmd);
   }
 
+
   // init
-  auto option = make_shared<json::value>();
-  auto job = make_shared<json::value>();
+  auto option = json::object();
+  auto job = json::object();
   auto sourceHash = make_shared<string>();
 
-  auto infileBuffer = make_shared<producer_consumer_buffer<unsigned char>>();
-  auto outfileBuffer = make_shared<producer_consumer_buffer<unsigned char>>();
-  auto tempBuffer = make_shared<producer_consumer_buffer<unsigned char>>();
+  stringstream infileBuffer;
+  auto outfileBuffer = make_shared<stringstream>();
+  auto tempBuffer = make_shared<stringstream>();
 
   string secc_scheduler_address = (getenv("SECC_ADDRESS")) ? getenv("SECC_ADDRESS") : "127.0.0.1";
   string secc_scheduler_port = (getenv("SECC_PORT")) ? getenv("SECC_PORT") : "10509";
   string secc_scheduler_host = "http://" + secc_scheduler_address + ":" + secc_scheduler_port; //FIXME : from settings
-  string secc_compiler = _basename(argv[0], false);                // FIXME : exception
-  string secc_compiler_path = "/usr/bin/" + secc_compiler;  // FIXME : from PATH
+  string secc_driver = _basename(argv[0], false);                // FIXME : exception
+  string secc_driver_path = "/usr/bin/" + secc_driver;  // FIXME : from PATH
   string secc_cwd = getcwd(nullptr, 0);
   string secc_mode = "1";      //FIXME : mode 2
   bool secc_cache = (getenv("SECC_CACHE") && strcmp(getenv("SECC_CACHE"),"1") == 0) ? true : false;
   bool secc_cross = (getenv("SECC_CROSS") && strcmp(getenv("SECC_CROSS"),"1") == 0) ? true : false;
 
   bool _argv_c_exists = false;
-  json::value secc_argv = json::value::array();
+  auto secc_argv = json::array();
   for(int i = 1; i < argc; i++) {
     if (strcmp(argv[i],"-c") == 0) _argv_c_exists = true;
-    secc_argv[i-1] = json::value(argv[i]);
+    secc_argv[i-1] = argv[i];
   }
 
-  //task
-  pplx::task<void> requestTask = pplx::create_task([=](){
+  try
+  {
     //quick checks.
     if (secc_scheduler_host.size() == 0) {
       LOGE("secc_scheduler_host error");
@@ -86,251 +86,195 @@ int main(int argc, char* argv[])
       throw secc_exception;
     }
 
-    http::client::http_client client(U(secc_scheduler_host));
-    uri_builder builder(U("/option/analyze"));
+    auto data = json::object();
+    data["driver"] = secc_driver;
+    data["cwd"] = secc_cwd;
+    data["mode"] = secc_mode;
+    data["argv"] = secc_argv;
 
-    json::value data = json::value::object();
-    data["compiler"] = json::value(secc_compiler);
-    data["cwd"] = json::value(secc_cwd);
-    data["mode"] = json::value(secc_mode);
-    data["argv"] = json::value(secc_argv);
+    LOGI(data.dump());
 
-    LOGI("REQUEST body /option/analyze");
-    LOGI(data);
-
-    return client.request(methods::POST, builder.to_string(), data);
-  })
-  .then([=](http_response response)
-  {
-    LOGI("option - response status code: ", response.status_code());
-    if (response.status_code() != 200)
+    SimpleHttpRequest requestOptionAnalyze;
+    requestOptionAnalyze.timeout = 50000;
+    requestOptionAnalyze.setHeader("content-type","application/json")
+    .post(secc_scheduler_host + "/option/analyze", data.dump())
+    .on("error", [](Error&& err){
       throw secc_exception;
+    }).on("response", [&](Response&& res){
+      LOGI("option - response status code: ", res.statusCode);
+      if (res.statusCode != 200)
+        throw secc_exception;
 
-    return response.extract_json();
-  })
-  .then([=](json::value result)
-  {
-    LOGI("RESPONSE body /option/analyze");
-    LOGI(result);
+      option = json::parse(res.str());
+      LOGI(option.dump());
 
-    *option = result;
-
-    //FIXME : check invalid the outfile dirname
-    if (option->at("useLocal").as_bool()) {
-      LOGE("useLocal from /option/analyze");
-      throw secc_exception;
-    }
-
-    //preprocessed file.
-    string cmd = secc_compiler_path;
-    for(size_t i = 0; i < option->at("localArgv").size(); i++)
-      cmd += " '" + option->at("localArgv")[i].as_string() + "'";
-
-    LOGI("COMMAND for generating a preprocessed source.");
-    LOGI(cmd);
-
-    //const char *cmd = "/usr/bin/gcc -c /mnt/sda1/open/secc_test/test/test.c -E";
-    size_t totalSize;
-    int ret = getZippedStream(cmd.c_str(), infileBuffer, sourceHash, &totalSize);
-    if (ret != 0)
-      throw secc_exception;
-
-    LOGI("request infile size : ", totalSize);
-
-    return pplx::create_task([=](){
-      return 1;
-    });
-  })
-  .then([=](int ret)
-  {
-    //system information
-    struct utsname u;
-    if (uname(&u) != 0)
-      throw secc_exception;
-
-    string hostname = u.nodename;
-    string platform = (strcmp(u.sysname,"Linux") == 0) ? "linux" : "unknown"; //FIXME : darwin
-    string release = u.release;
-    string arch = (strcmp(u.machine,"x86_64") == 0) ? "x64" : "unknown"; //FIXME : arm
-
-    string compiler_version = _exec(string(secc_compiler_path + " --version").c_str());
-    string compiler_dumpversion = _exec(string(secc_compiler_path + " -dumpversion").c_str());
-    string compiler_dumpmachine = _exec(string(secc_compiler_path + " -dumpmachine").c_str());
-    compiler_dumpversion = trim(compiler_dumpversion);
-    compiler_dumpmachine = trim(compiler_dumpmachine);
-
-    json::value data = json::value::object();
-    data["systemInformation"] = json::value::object();
-    data["systemInformation"]["hostname"] = json::value(hostname);
-    data["systemInformation"]["platform"] = json::value(platform);
-    data["systemInformation"]["release"] = json::value(release);
-    data["systemInformation"]["arch"] = json::value(arch);
-    data["compilerInformation"] = json::value::object();
-    data["compilerInformation"]["version"] = json::value(compiler_version);
-    data["compilerInformation"]["dumpversion"] = json::value(compiler_dumpversion);
-    data["compilerInformation"]["dumpmachine"] = json::value(compiler_dumpmachine);
-    data["mode"] = json::value(secc_mode);
-    data["projectId"] = option->at("projectId");
-    data["cachePrefered"] = json::value(secc_cache);
-    data["crossPrefered"] = json::value(secc_cross);
-    data["sourcePath"] = option->at("infile");
-    data["sourceHash"] = json::value(*sourceHash);
-    data["argvHash"] = option->at("argvHash");
-
-    LOGI("REQUEST body /job/new");
-    LOGI(data);
-
-    http::client::http_client client(U(secc_scheduler_host));
-    uri_builder builder(U("/job/new"));
-
-    return client.request(methods::POST, builder.to_string(), data);
-  })
-  .then([=](http_response response){
-    LOGI("JOB - response status code:", response.status_code());
-
-    if (response.status_code() != 200)
-      throw secc_exception;
-
-    return response.extract_json();
-  })
-  .then([=](json::value result)
-  {
-    LOGI("RESPONSE body /job/new");
-    LOGI(result);
-
-    *job = result;
-
-    if (job->at("local").as_bool()) {
-      LOGE("useLocal from SCHEDULER /job/new");
-      throw secc_exception;
-    }
-
-    // try cache! FIXME : refactoring!
-    if (secc_cache && job->at("cache").as_bool()) {
-      try {
-        string secc_daemon_host = "http://" + job->at("daemon")["daemonAddress"].as_string() + ":" + std::to_string(job->at("daemon")["system"]["port"].as_integer());
-        string secc_daemon_cache_uri = secc_daemon_host + "/cache/" + job->at("archive")["archiveId"].as_string() + "/" + *sourceHash + "/" + option->at("argvHash").as_string();
-
-        LOGI("cache is available. try URL : ", secc_daemon_cache_uri);
-        http::client::http_client client(U(secc_daemon_cache_uri));
-        http_request msg(methods::GET);
-
-        http_response response = client.request(msg).get();
-        if (response.status_code() != 200)
-          throw secc_exception;
-
-        response.body().read_to_end(*outfileBuffer).get();
-        outfileBuffer->close(std::ios_base::out).get();
-
-        string outdir = (option->at("outfile").is_null())
-                      ? secc_cwd
-                      : _dirname(option->at("outfile").as_string());
-
-        LOGI("outdir : ", outdir);
-
-        auto zipStreambuf = outfileBuffer->create_istream().streambuf();
-        auto oTarStreambuf = tempBuffer->create_ostream().streambuf();
-
-        int ret = unzip(&zipStreambuf, &oTarStreambuf);
-        if (ret != 0)
-          throw secc_exception;
-
-        LOGI("unzip done.");
-
-        auto isTar = tempBuffer->create_istream();
-        concurrency::streams::streambuf<char> iTarStreambuf = isTar.streambuf();
-
-        ret = untar(&iTarStreambuf, outdir.c_str());
-        if (ret != 0)
-          throw secc_exception;
-
-        LOGI("cache done");
-        _exit(0);
-      } catch(const std::exception &e) {
-        LOGE("failed to hit cache.");
+      //FIXME : check invalid the outfile dirname
+      if (option["useLocal"].get<bool>()) {
+        LOGE("useLocal from /option/analyze");
+        throw secc_exception;
       }
-    }
 
-    string secc_daemon_host = "http://" + job->at("daemon")["daemonAddress"].as_string() + ":" + std::to_string(job->at("daemon")["system"]["port"].as_integer());
-    string secc_daemon_compile_uri = secc_daemon_host + "/compile/preprocessed/" + job->at("archive")["archiveId"].as_string();
+      //preprocessed file.
+      string cmd = secc_driver_path;
+      for(size_t i = 0; i < option["localArgv"].size(); i++)
+        cmd += " '" + option["localArgv"][i].get<string>() + "'";
 
-    string secc_filename = option->at("outfile").is_null()
-                              ? _basename(option->at("infile").as_string(), true)
-                              : _basename(option->at("outfile").as_string(), true);
+      LOGI("COMMAND for generating a preprocessed source.");
+      LOGI(cmd);
 
-    http::client::http_client_config client_config;
-    client_config.set_timeout(utility::seconds(60));
-    http::client::http_client client(U(secc_daemon_compile_uri), client_config);
-    http_request msg(methods::POST);
-    msg.headers().add(U("Content-Encoding"), U("gzip"));
-    msg.headers().add(U("secc-jobid"), job->at("jobId"));
-    msg.headers().add(U("secc-compiler"), secc_compiler);
-    msg.headers().add(U("secc-language"), option->at("language"));
-    msg.headers().add(U("secc-argv"), option->at("remoteArgv"));
-    msg.headers().add(U("secc-filename"), secc_filename);
-    msg.headers().add(U("secc-cross"), secc_cross);
-    msg.headers().add(U("secc-target"), U("x86_64-linux-gnu")); // FIXME : from system
+      //const char *cmd = "/usr/bin/gcc -c /mnt/sda1/open/secc_test/test/test.c -E";
+      size_t totalSize;
+      int ret = getZippedStream(cmd.c_str(), infileBuffer, sourceHash, &totalSize);
+      if (ret != 0)
+        throw secc_exception;
 
-    LOGI("REQUEST headers ",secc_daemon_compile_uri);
-    LOGI(msg.to_string());
+      LOGI("request infile size : ", totalSize);
 
-    msg.set_body(infileBuffer->create_istream());
 
-    return client.request(msg);
-  })
-  .then([=](http_response response)
-  {
-    //check secc-code
-    LOGI("compile - response status code: ", response.status_code());
-    if (response.status_code() != 200)
-      throw secc_exception;
+      //system information
+      struct utsname u;
+      if (uname(&u) != 0)
+        throw secc_exception;
 
-    LOGI("RESPONSE headers ");
-    LOGI(response.to_string());
 
-    return response.body().read_to_end(*outfileBuffer);
-  })
-  .then([=](size_t c)
-  {
-    LOGI("response outfile size : ", c);
+      string hostname = u.nodename;
+      string platform = (strcmp(u.sysname,"Linux") == 0) ? "linux" : "unknown"; //FIXME : darwin
+      string release = u.release;
+      string arch = (strcmp(u.machine,"x86_64") == 0) ? "x64" : "unknown"; //FIXME : arm
 
-    return outfileBuffer->close(std::ios_base::out);
-  })
-  .then([=](){
-    string outdir = (option->at("outfile").is_null())
-                  ? secc_cwd
-                  : _dirname(option->at("outfile").as_string());
-    LOGI("outdir : ", outdir);
+      string compiler_version = _exec(string(secc_driver_path + " --version").c_str());
+      string compiler_dumpversion = _exec(string(secc_driver_path + " -dumpversion").c_str());
+      string compiler_dumpmachine = _exec(string(secc_driver_path + " -dumpmachine").c_str());
+      compiler_dumpversion = trim(compiler_dumpversion);
+      compiler_dumpmachine = trim(compiler_dumpmachine);
 
-    auto zipStreambuf = outfileBuffer->create_istream().streambuf();
-    auto oTarStreambuf = tempBuffer->create_ostream().streambuf();
+      auto data = json::object();
+      data["systemInformation"] = json::object();
+      data["systemInformation"]["hostname"] = hostname;
+      data["systemInformation"]["platform"] = platform;
+      data["systemInformation"]["release"] = release;
+      data["systemInformation"]["arch"] = arch;
+      data["compilerInformation"] = json::object();
+      data["compilerInformation"]["version"] = compiler_version;
+      data["compilerInformation"]["dumpversion"] = compiler_dumpversion;
+      data["compilerInformation"]["dumpmachine"] = compiler_dumpmachine;
+      data["mode"] = secc_mode;
+      data["projectId"] = option["projectId"];
+      data["cachePrefered"] = secc_cache;
+      data["crossPrefered"] = secc_cross;
+      data["sourcePath"] = option["infile"];
+      data["sourceHash"] = *sourceHash;
+      data["argvHash"] = option["argvHash"];
 
-    int ret = unzip(&zipStreambuf, &oTarStreambuf);
-    if (ret != 0)
-      throw secc_exception;
+      LOGI("REQUEST body /job/new");
+      LOGI(data);
 
-    LOGI("unzip done.");
-    concurrency::streams::streambuf<char> iTarStreambuf = tempBuffer->create_istream().streambuf();
+      SimpleHttpRequest requestJobNew;
+      requestJobNew.timeout = 50000;
+      requestJobNew.setHeader("content-type","application/json")
+      .post(secc_scheduler_host + "/job/new", data.dump())
+      .on("error", [](Error&& err){
+        throw secc_exception;
+      }).on("response", [&](Response&& res){
+        LOGI("JOB - response status code:", res.statusCode);
 
-    ret = untar(&iTarStreambuf, outdir.c_str());
-    if (ret != 0)
-      throw secc_exception;
+        if (res.statusCode != 200)
+          throw secc_exception;
 
-    LOGI("done");
-  });
+        job = json::parse(res.str());
+        LOGI(job.dump());
 
-  try
-  {
-    requestTask.wait();
+        if (job["local"].get<bool>()) {
+          LOGE("useLocal from SCHEDULER /job/new");
+          throw secc_exception;
+        }
+
+        // FIXME : implement CACHE logic!!
+        //
+
+        string secc_daemon_host = "http://" + job["daemon"]["daemonAddress"].get<string>() + ":" + std::to_string(job["daemon"]["daemonPort"].get<int>());
+        string secc_daemon_compile_uri = secc_daemon_host + "/compile/preprocessed/" + job["archive"]["archiveId"].get<string>();
+
+        string secc_filename = option["outfile"].is_null()
+                                  ? _basename(option["infile"], true)
+                                  : _basename(option["outfile"], true);
+
+        // FIXME : using write() with map<string, string> options
+        //                        and map<string, string> headers
+        LOGI("REQUEST headers ");
+        //LOGI(headers)
+        //LOGI(infileBuffer.str());
+
+        stringstream body;
+        body << "{\"archive\":3}";
+
+        SimpleHttpRequest request;
+        request.timeout = 50000;
+        request
+        .setHeader("content-type","application/octet-stream")
+        .setHeader("Content-Encoding", "gzip")
+        .setHeader("secc-jobid", to_string(job["jobId"].get<int>()))
+        .setHeader("secc-driver", secc_driver)
+        .setHeader("secc-language", option["language"])
+        .setHeader("secc-argv", option["remoteArgv"].dump())
+        .setHeader("secc-filename", secc_filename)
+        .setHeader("secc-cross", secc_cross ? "true" : "false")
+        .setHeader("secc-target", "x86_64-linux-gnu") // FIXME : from system
+        .post(secc_daemon_compile_uri, infileBuffer.str())
+        .on("error", [](Error&& err){
+          throw secc_exception;
+        });
+        request.on("response", [&](Response&& res){
+          //check secc-code
+          LOGI("compile - response status code: ", res.statusCode);
+          if (res.statusCode != 200)
+            throw secc_exception;
+
+          LOGI("RESPONSE headers ");
+          for (const auto &kv : res.headers)
+            LOGI(kv.first, " : ", kv.second);
+
+          //cout << res.str() << endl;
+
+          string outdir = (option["outfile"].is_null())
+                        ? secc_cwd
+                        : _dirname(option["outfile"].get<string>());
+          LOGI("outdir : ", outdir);
+
+
+          stringstream tarStream;
+
+          int ret = unzip(res, tarStream);
+          if (ret != 0)
+            throw secc_exception;
+
+          LOGI("unzip done.");
+          ret = untar(tarStream, outdir.c_str());
+          if (ret != 0)
+            throw secc_exception;
+
+          LOGI("done");
+
+        });
+        request.end();
+
+      }).end();
+
+    })
+    .end();
+
   }
   catch (const std::exception &e)
   {
     LOGE("Error exception: ", e.what());
-    LOGI("passThrough ", secc_compiler_path.c_str());
+    LOGI("passThrough ", secc_driver_path.c_str());
 
     //passThrough
-    strcpy(argv[0], secc_compiler_path.c_str());
-    execv(secc_compiler_path.c_str(), argv);
+    char pathBuf[1024];
+    strcpy(pathBuf, secc_driver_path.c_str());
+    argv[0] = pathBuf;
+
+    execv(secc_driver_path.c_str(), argv);
   }
 
   return 0;
--- utils.h
+++ utils.h
@@ -5,9 +5,35 @@
 
 using namespace std;
 
-std::string _exec(const char* cmd);
-std::string _basename(const std::string &path, bool removeExt);
-std::string _dirname(const std::string &path);
+std::string _basename(const std::string &path, bool removeExt)
+{
+  size_t i = path.find_last_of('/');
+  std::string base = (i == std::string::npos) ? path : path.substr(i +1);
+  if (!removeExt)
+    return base;
+
+  i = base.find_last_of(".");
+  return (i == std::string::npos) ? base : base.substr(0, i);
+}
+
+std::string _dirname(const std::string &path)
+{
+  size_t i = path.find_last_of('/');
+  return (i == std::string::npos) ? "." : path.substr(0, i);
+}
+
+std::string _exec(const char* cmd) {
+  std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+  if (!pipe) return nullptr;
+  char buffer[128];
+  std::string result = "";
+  while (!feof(pipe.get())) {
+    if (fgets(buffer, 128, pipe.get()) != NULL)
+      result += buffer;
+  }
+  return result;
+}
+
 
 static inline std::string &ltrim(std::string &s) {
   s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
